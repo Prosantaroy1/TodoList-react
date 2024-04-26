88629a3716880f6335b380857c60efec771b69e5
Prosantaroy1 <royprosanta172@gmail.com>
2024-04-26 12:04:17 +0100

    todolist setup done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h3 className='text-center'>TODO LIST APP YOUR TEXT SAVE ZONE!</h3>
+    </div>
+  )
+}
+
+export default App
